0e16f245795de1d6564f9bb26b9788eb8cd19cc5
Shawn Ohn <developer.ohn@gmail.com>
2021-06-18 15:57:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const sortCategoriesForInsert = require('./sortCategoriesForInsert.js');
+
+const arg_json = process.argv[2];
+const arg_out = process.argv[3];
+
+fs.readFile(arg_json, 'utf8', (err, data) => {
+  if (err) {
+      console.log(`Can not read Input: ${err.message}`);
+      process.exit;
+    }
+  var jsonInput = JSON.parse(data);
+  
+  var result = sortCategoriesForInsert(jsonInput);
+
+  var jsonOutput = JSON.stringify(result);
+
+  fs.writeFile(arg_out, jsonOutput, "utf8", (err) => {
+    if (err) {
+      console.log(`Can not write JSON: ${err.message}`);
+      process.exit;
+    }
+    console.log("JSON write end");
+  });
+});
---
+++ sortCategoriesForInsert.js
@@ -0,0 +1,41 @@
+module.exports = function sortCategoriesForInsert (inputJson) {
+
+    // Declear Hash Table { Key: parent_id, value: [children] }
+    var hashTable = [];
+
+    // To use Hash Table properly, replace null of parent_id to 0
+    var arr = inputJson.map((item) => {   
+        item.parent_id = item.parent_id === null ? 0 : item.parent_id;
+        return item;
+    });
+    
+    // Implement Hash Table
+    for (var i = 0; i < arr.length; i++) {
+        if (hashTable[arr[i].parent_id] == undefined) {
+            hashTable[arr[i].parent_id] = [];
+        }
+        hashTable[arr[i].parent_id].push(arr[i]);
+    }
+
+    // Sort Hash Table
+    var properJsonOutput = hierarhySort(hashTable, 0, []);
+
+    // Rollback 0 to null
+    properJsonOutput.map((item) => {   
+        item.parent_id = item.parent_id === 0 ? null : item.parent_id;
+    });
+
+    return properJsonOutput;
+}
+
+// Recursive function to sort by parent and child
+function hierarhySort(hashTable, key, result) {     
+    if (hashTable[key] == undefined) return;
+
+    var arr = hashTable[key];
+    for (var i = 0; i < arr.length; i++) {
+      result.push(arr[i]);
+      hierarhySort(hashTable, arr[i].id, result);
+    }
+    return result;
+}
